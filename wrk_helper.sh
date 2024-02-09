a50d572f4ae242f00183ba133613d95ef5343996
Danil Duk <danil.duck@gmail.com>
2024-02-09 22:35:12 +0200

    verbose

--- wrk_helper.sh
+++ wrk_helper.sh
@@ -7,19 +7,21 @@ DEFAULT_CONNECTIONS_VALUES=1
 DEFAULT_DURATION=30
 DEFAULT_RPS_VALUES=100
 DEFAULT_URL=
+DEFAULT_VERBOSE=0
 
 show_help() {
     cat <<EOF
 Usage: $0 [OPTIONS] <url>
 
 Options:
-  -h             Display this help message
-  -t <threads>   Number of threads (default: $DEFAULT_THREADS)
-  -c <connections>  Number of connections (default: $DEFAULT_CONNECTIONS_VALUES)
-  -R <RPS>       Requests per second (default: $DEFAULT_RPS_VALUES)
-  -d <duration>  Test duration (default: $DEFAULT_DURATION)
-  -s <sleep>     Sleep between iterations (default: $DEFAULT_SLEEP_DURATION)
-  -p <path>      Path to wrk executable (default: $DEFAULT_WRK_EXEC_PATH)
+  -h                Display this help message
+  -t <threads>      Number of threads (default: $DEFAULT_THREADS)
+  -c <connections>  Array of numbers of connections (eg: "50,100,1000") (default: $DEFAULT_CONNECTIONS_VALUES)
+  -R <RPS>          Array of requests per second (eg: "50,100,1000") (default: $DEFAULT_RPS_VALUES)
+  -d <duration>     Test duration (default: $DEFAULT_DURATION)
+  -s <sleep>        Sleep between iterations (default: $DEFAULT_SLEEP_DURATION)
+  -p <exec path>    Path to wrk executable (default: $DEFAULT_WRK_EXEC_PATH)
+  -v <verbose>      Duplicate wrk output to console
 EOF
 }
 
@@ -30,8 +32,9 @@ connections_values="$DEFAULT_CONNECTIONS_VALUES"
 duration="$DEFAULT_DURATION"
 rps_values="$DEFAULT_RPS_VALUES"
 url="$DEFAULT_URL"
+verbose="$DEFAULT_VERBOSE"
 
-while getopts ":ht:t:c:d:R:s:p:" opt; do
+while getopts ":ht:t:c:d:R:s:p:vt:" opt; do
     case $opt in
         h) show_help; exit 0 ;;
         t) threads=$OPTARG;;
@@ -40,6 +43,7 @@ while getopts ":ht:t:c:d:R:s:p:" opt; do
         R) rps_values=$OPTARG;;
         s) sleep_duration=$OPTARG;;
         p) wrk_exec_path=$OPTARG;;
+        v) verbose=1;;
         \?) echo "Invalid option: -$OPTARG" >&2; exit 1;;
         :)  echo "Option -$OPTARG requires an argument." >&2; exit 1;;
     esac
@@ -62,19 +66,34 @@ if [ ${#connections_values[@]} -eq 0 ] || [ ${#rps_values[@]} -eq 0 ]; then
 fi
 
 safe_url=$(echo "$url" | sed 's/[^a-zA-Z0-9]/_/g')
+i=0
 for connections in "${connections_values[@]}"; do
-  output_file="$(date '+%Y_%m_%d_%H_%M_%S')-$safe_url-c-$connections.txt"
+  output_file="$(date '+%Y_%m_%d_%H_%M_%S')-$safe_url-c$connections.txt"
   echo "$output_file"
   touch "$output_file"
 
   for rps in "${rps_values[@]}"; do
+    ((i++))
     echo "Running test with $connections connections and $rps RPS..."
     command="$wrk_exec_path -t$threads -c$connections -R$rps -d$duration -L $url"
     echo "$command" >> "$output_file"
-    $command 2>&1 | tee -a "$output_file"
+    if [ "$verbose" = "1" ]; then
+      $command 2>&1 | tee -a "$output_file"
+    else
+      output=$($command 2>&1)
+      if [ $? -ne 0 ]; then
+        echo "$output"
+        echo "$output" >> "$output_file"
+      else
+        echo "$output" >> "$output_file"
+      fi
+    fi
     echo "-------------------------------------------" >> "$output_file"
-    echo "Test completed. Sleep $sleep_duration"
-    sleep "$sleep_duration"
+
+    if [ "$i" -ne "$(( ${#connections_values[@]} * ${#rps_values[@]} ))" ]; then
+      echo "Test completed. Sleep $sleep_duration"
+      sleep "$sleep_duration"
+    fi
     echo "-------------------------------------------"
   done
 done
